277ea16bd6eb0388803e265f41182ffeaa27d53d
pauloSergioMorais <119258008+pauloSergioMorais@users.noreply.github.com>
2023-09-12 13:58:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+let canvas = document.getElementById("signature-pad");
+
+function resizeCanvas( ) {
+  let ratio = Math.max(window.devicePixelRatio || 1, 1);
+  canvas.width = canvas.offsetWidth * ratio;
+  canvas.height = canvas.offsetHeight * ratio;
+  canvas.getContext("2d").scale(ratio, ratio);
+}
+
+window.onresize = resizeCanvas;
+resizeCanvas();
+
+let signaturePad = new SignaturePad(canvas, {
+  backgroundColor: "rgb(255, 255, 255)"
+  
+});
+
+document.getElementById("clear").addEventListener("click",function () {
+  signaturePad.clear(); 
+});
